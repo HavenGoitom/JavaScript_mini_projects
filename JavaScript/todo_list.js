21284038aabc616487268346eae3e6d9e1c51ce2
HavenGoitom <havengoitom2@gmail.com>
2025-09-01 20:16:11 +0300

    finished with the todo list app

--- JavaScript/todo_list.js
+++ JavaScript/todo_list.js
@@ -1,11 +1,31 @@
-let todos = []
-function AddTodos(){
-    let inputElement = document.querySelector('.js-name-input');
-    let todo = inputElement.value;
-    todos.push(String(todo)); 
-    todo_result = document.querySelector('.js-name-output');
-    todo_result.innerHTML = "";
-    for(i=0; i<todos.length; i++){
-        todo_result.innerHTML += todos[i] + "<br>"
+let todos = [];
+
+function renderTodoList() {
+    let todoListHtml = '';
+    for (let i = 0; i < todos.length; i++) {
+        const todo = todos[i];
+        const html = `
+        <div class="todo">${todo.name}</div>
+        <div class="todo-date">${todo.date}</div>
+        <button class="todos-delete" onclick="
+            todos.splice(${i}, 1);
+            renderTodoList();
+        ">Delete</button>`;
+        todoListHtml += html;
+    }
+    document.querySelector('.js-name-output').innerHTML = todoListHtml;
+}
+
+function AddTodos() {
+    let nameInput = document.querySelector('.js-name-input');
+    let dateInput = document.querySelector('.js-date-input');
+    let name = nameInput.value.trim();
+    let date = dateInput.value;
+
+    if (name && date) {
+        todos.push({ name: name, date: date });
+        renderTodoList();
+        nameInput.value = '';
+        dateInput.value = '';
     }
 }

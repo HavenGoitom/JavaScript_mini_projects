2d010ffc307a816f8164767abd39e3cb7b3d55a1
HavenGoitom <havengoitom2@gmail.com>
2025-09-24 14:57:06 +0300

    Updated the data and regenerated all the html(MVC)

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -3,8 +3,9 @@ import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
 let productsHTML = '';
-products.forEach((product)=>{
-    productsHTML += `
+
+products.forEach((product) => {
+  productsHTML += `
     <div class="product-container">
       <div class="product-image-container">
         <img class="product-image"
@@ -17,7 +18,7 @@ products.forEach((product)=>{
 
       <div class="product-rating-container">
         <img class="product-rating-stars"
-          src="images/ratings/rating-${product.rating.stars *10}.png">
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
         <div class="product-rating-count link-primary">
           ${product.rating.count}
         </div>
@@ -50,28 +51,31 @@ products.forEach((product)=>{
       </div>
 
       <button class="add-to-cart-button button-primary js-add-to-cart"
-      data-product-id = "${product.id}">
+      data-product-id="${product.id}">
         Add to Cart
       </button>
     </div>
-    `;
-})
+  `;
+});
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-function updateCartQuantity(){
+function updateCartQuantity() {
   let cartQuantity = 0;
-    cart.forEach((cartItem) =>{
-      cartQuantity += cartItem.quantity;
-    });
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-}
-  
 
-document.querySelectorAll('.js-add-to-cart').forEach((button) => {
-  button.addEventListener('click', ()=> {
-    const productId = button.dataset.productId;
-    addToCart(productId);
-    updateCartQuantity();
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
   });
-});
+
+  document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
+}
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+      addToCart(productId);
+      updateCartQuantity();
+    });
+  });
--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -1,10 +1,10 @@
-import {cart, removeFromCart, updateDeliveryOption} from '../data/cart.js';
-import {products, getProduct} from '../data/products.js';
-import {formatCurrency} from './utils/money.js';
+import {cart, removeFromCart, updateDeliveryOption} from '../../data/cart.js';
+import {products, getProduct} from '../../data/products.js';
+import {formatCurrency} from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import {deliveryOptions, getDeliveryOption} from '../data/deliveryOptions.js';
-import {renderPaymentSummary} from './checkout/paymentSummary.js';
+import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
+import {renderPaymentSummary} from './paymentSummary.js';
 
 export function renderOrderSummary() {
   let cartSummaryHTML = '';
@@ -139,5 +139,3 @@ export function renderOrderSummary() {
       });
     });
 }
-
-renderOrderSummary();
